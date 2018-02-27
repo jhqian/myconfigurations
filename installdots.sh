c2a8926916e35b77f1433c940a7880254b9b0dda
Ryan QIAN <jianghao.qian@nxp.com>
2018-02-27 21:31:46 +0800

    remove existing files
    Signed-off-by: Ryan QIAN <jianghao.qian@nxp.com>

--- installdots.sh
+++ installdots.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+function remove()
+{
+    if [ -L $HOME/.${rcfile##*/} ]; then
+        unlink $HOME/.${rcfile##*/}
+    elif [ -e $HOME/.${rcfile##*/} ]; then
+        rm -f $HOME/.${rcfile##*/}
+    fi
+}
+
 for rcfile in dots/*; do
+    remove $HOME/.${rcfile##*/}
     ln -s `pwd`/$rcfile $HOME/.${rcfile##*/}
 done
 
